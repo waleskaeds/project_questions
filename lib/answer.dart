36015bb3c70acbbba1485d2bdbd67e478f146323
waleskaeds <waleskaeds05@gmail.com>
2023-03-18 23:49:57 -0300

    condição ternária

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,27 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Answer extends StatelessWidget {
   final String texto;
+  final void Function() onSelect;
 
-  Answer(this.texto);
+  const Answer(this.texto, {super.key, required this.onSelect});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-      color: Colors.deepPurpleAccent,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
       ),
       child: ElevatedButton(
-        onPressed: () {}, 
+        onPressed: onSelect, 
         child: Text(
           texto,
           style: const TextStyle(
-            color: Colors.white
+            color: Colors.white,
           ),
         ))
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:project_questions/question.dart';
-
 import 'answer.dart';
+
 
 main(){
   runApp(QuestionsApp());
 }
 
 class _QuestionsAppState extends State<QuestionsApp> {
-   var _questionSelect = 0; 
+  var _questionSelect = 0;  
+  final List<Map<String, Object>> _questions = [
+     {
+       'question': 'qual é sua cor favorita?',
+       'answer'  : ['azul', 'roxo', 'rosa', 'amarelo'] 
+     },
+     {
+       'question': 'qual é seu animal favorito',
+       'answer'  : ['cachorro', 'gato', 'calopsita', 'coelho']
+     },
+     {
+       'question': 'qual é sua comida favorita',
+       'answer'  : ['pizza', 'hamburguer', 'sushi', 'mac cheese']
+     }
+   ];
+  
+   get haveQuestionSelect => _questionSelect < _questions.length; 
 
-  void _responder() {
-    setState(() {
-      _questionSelect ++;
-    });
-    print(_questionSelect);
+   void _responder() {
+     setState(() {
+       _questionSelect ++;
+     });
+
   }
 
   @override
   Widget build(BuildContext context){
-    
-    final questions = [
-    'qual é sua cor favorita?',
-    'qual é seu animal favorito',
-    'qual é sua comida favorita',
-    ];
+
+    List<String> answers = haveQuestionSelect 
+    ?_questions[_questionSelect].cast()['answer']
+    : [];
+
+    // for(String textAnswer in answers){
+    //   widgets.add(Answer(textAnswer, onSelect: () {_responder();})
+    //   );
+    // }
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
-        body: Column(
+        body: haveQuestionSelect 
+        ?Column(
           children: <Widget>[
-            Question(questions[_questionSelect]),
-            Answer('resposta 1'),
-            Answer('resposta 2'),
-            Answer('resposta 3'),
+            Question(_questions[_questionSelect]['question'].toString()),
+            ...answers.map((t) => Answer(t, onSelect: (){_responder();})).toList()
           ],
-        ),
+        )
+        : null,
       ),
     );
   }
